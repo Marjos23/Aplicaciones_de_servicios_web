a77b6fac5f1e2d7f988d3b209cf5a274e012af66
Marjos23 <jvicenteontaneda110@gmail.com>
2026-09-08 14:36:13 -0500

    feat: add course read endpoints

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,7 @@ import { AppController } from './app.controller.js'; // 2
 import { AppService } from './app.service.js'; // 3
 import { WelcomeController } from './welcome.controller.js'; // 4
 import { WelcomeService } from './welcome.service.js'; // 5
+import { CoursesModule } from './courses/courses.module.js';
 
 export const { ObserveModule, ObserveInstrument } = createObserveModule();
 
@@ -14,6 +15,7 @@ export const { ObserveModule, ObserveInstrument } = createObserveModule();
       appSecret: process.env.OBSERVE_APP_SECRET ?? '',
       serviceId: process.env.OBSERVE_SERVICE_ID ?? 'project-name',
     }),
+    CoursesModule,
   ], // 7
   controllers: [AppController, WelcomeController], // 8
   providers: [AppService, WelcomeService], // 9
